9b50358042557ea86705f6987cf144a4d56e9ece
David Shaw <mx496@nyu.edu>
2020-04-10 21:09:14 -0400

    V3 Skeleton

--- PubMed.py
+++ PubMed.py
@@ -3,17 +3,54 @@ import urllib.request
 from bs4 import BeautifulSoup
 from concurrent.futures import ThreadPoolExecutor
 from Bio import Entrez
-import numpy as np
-import os
 import traceback
+import sqlite3
 
-max_retry = 3
+max_retry = 1
+date_format = ['Year', 'Month', 'Day']
+
+# create database
+def setup_database():
+	conn = None
+	try:
+		conn = sqlite3.connect('./pubmed.db')
+		# print(sqlite3.version)
+	except Error as e:
+		print(e)
+	finally:
+		if conn:
+			c = conn.cursor()
+			c.execute("PRAGMA foreign_keys = ON")
+			c.execute('''
+				CREATE TABLE IF NOT EXISTS PubMed(
+					TITLE varchar(512) NOT NULL,
+					K1 varchar(128) NOT NULL,
+					K2 varchar(128) NOT NULL,
+					DOI varchar(256),
+					AUTHORS varchar(500),
+					TYPE varchar(20),
+					PUBLICATIONYEAR var(4),
+					URL varchar(512),
+					PRIMARY KEY (TITLE)
+				)
+			''')
+			c.execute('''
+        		CREATE TABLE IF NOT EXISTS PubMed_Abstract(
+        			TITLE varchar(512) NOT NULL REFERENCES PubMed(TITLE) ON DELETE CASCADE,
+        			K1 varchar(128),
+        			K2 varchar(128),
+        			ABSTRACT text,
+					PRIMARY KEY (TITLE)
+        		)
+			''')
+			conn.commit()
+			conn.close()
 
 # extract and index keywords
 def keyword(filepath='./Keywords for Systematic Review Analysis for Trauma by DV-01-29-2020.docx'):
     import docx
     f = docx.Document(filepath)
-    first_keyword, second_keyword, exclude = {}, {}, []
+    first_keyword, second_keyword, exclude = [], [], []
     container, i, j, count = None, [0], [0], None
 
     # extract keywords and divide them based on which set they belong to
@@ -30,21 +67,12 @@ def keyword(filepath='./Keywords for Systematic Review Analysis for Trauma by DV
     		exclude = t.text[t.text.index(':')+2:].split(', ')
     		break
     	else:
-    		container[count[0]] = t.text.strip().lower()
-    		count[0] += 1
+    		container.append(t.text.strip().lower())
     
     return first_keyword, second_keyword, exclude
 
-# after multithreading finished, extract results
-def unpack(mat):
-	for i in range(mat.shape[0]):
-		for j in range(1, mat.shape[1]):
-			if type(mat[i][j]) != int:
-				mat[i][j] = mat[i][j].result()
-	return mat
-
 # worker function for multithreading
-def worker(first, second, filepath):
+def worker(first, second):
 	count = 0
 	url = 'https://www.ncbi.nlm.nih.gov/pubmed?term=({}) AND {}'.format(first, second)
 
@@ -76,29 +104,50 @@ def worker(first, second, filepath):
 				if len(id_list) > 0:
 					handle = Entrez.efetch(db='pubmed',
 				                           retmode='xml',
-				                           id=id_list[0])
+				                           id=id_list)
 					r = Entrez.read(handle)['PubmedArticle']
 					if r:
-						with open('{}/index.txt'.format(filepath), 'w', encoding='utf-8') as f:
-							with open('{}/download_links.txt'.format(filepath), 'w', encoding='utf-8') as f2:
-								remaining = 0
-								while remaining < count:
-									handle = Entrez.efetch(db='pubmed',
-								                           retmode='xml',
-								                           id=','.join(id_list[remaining:]))
-									results = Entrez.read(handle)
-									for r in results['PubmedArticle']:
-										f.write("<TIL>{}\n<ABS>{}\n<PMID>{}\n<DOI>{}\n".format(
-											r['MedlineCitation']['Article']['ArticleTitle'],
-											' '.join([str(x) for x in r['MedlineCitation']['Article']['Abstract']['AbstractText']]) if 'Abstract' in r['MedlineCitation']['Article'] else '',
-											r['PubmedData']['ArticleIdList'][0],
-											str(r['MedlineCitation']['Article']['ELocationID'][-1]) if 'ELocationID' in r['MedlineCitation']['Article'] and r['MedlineCitation']['Article']['ELocationID'] else ''
-										))
-										f2.write('{}: {}\n'.format(
-											r['MedlineCitation']['Article']['ArticleTitle'], 
-											'sci-hub.tw/{}'.format(str(r['MedlineCitation']['Article']['ELocationID'][-1])) if 'ELocationID' in r['MedlineCitation']['Article'] and r['MedlineCitation']['Article']['ELocationID'] else ''
-										))
-									remaining += len(results['PubmedArticle'])
+						# connect to database
+						conn = sqlite3.connect('./pubmed.db')
+						c = conn.cursor()
+						remaining = 0
+						while remaining < count:
+							handle = Entrez.efetch(db='pubmed',
+						                           retmode='xml',
+						                           id=','.join(id_list[remaining:]))
+							results = Entrez.read(handle)
+							for r in results['PubmedArticle']:
+								# collect information
+								title = str(r['MedlineCitation']['Article']['ArticleTitle'])
+								# print('<title>', title)
+								abstract = ' '.join([str(x) for x in r['MedlineCitation']['Article']['Abstract']['AbstractText']]) if 'Abstract' in r['MedlineCitation']['Article'] else None
+								# print('<abstract>', abstract)
+								doi = None
+								for x in r['PubmedData']['ArticleIdList']:
+									if x.attributes['IdType'] == 'doi':
+										doi = x
+								# print('<doi>', doi)
+								url = 'http://sci-hub.tw/{}'.format(doi) if doi else None
+								authors = ['{} {}'.format(x['LastName'], x['ForeName']) for x in r['MedlineCitation']['Article']['AuthorList']] if 'AuthorList' in r['MedlineCitation']['Article'] else None # [last name] [first name]
+								# print('<authors>', authors)
+								article_type = str(r['MedlineCitation']['Article']['PublicationTypeList'][0])
+								# print('<article_type:', article_type)
+								publication_year = r['PubmedData']['History'][0]['Year']
+								# print('<publication_year>', publication_year)
+								# test url
+								if url:
+									response = requests.head(url)
+									if not response.ok: 
+										url = ''
+								# insert into database
+								c.execute('INSERT OR IGNORE INTO PubMed(TITLE, K1, K2, DOI, AUTHORS, TYPE, PUBLICATIONYEAR, URL) VALUES (?, ?, ?, ?, ?, ?, ?, ?)', 
+									(title, first, second, doi, ','.join(authors) if authors else authors, article_type, publication_year, url))
+								c.execute('INSERT OR IGNORE INTO PubMed_Abstract(TITLE, K1, K2, ABSTRACT) VALUES (?, ?, ?, ?)', 
+									(title, first, second, abstract))
+								conn.commit()
+								# print()
+							remaining += len(results['PubmedArticle'])
+						conn.close()
 					else:
 						count = 0
 				else:
@@ -107,66 +156,66 @@ def worker(first, second, filepath):
 				return count
 			except Exception as e:
 				print('WARNING: Retrying keywords "{}" and "{}": {}\n'.format(first, second, e), end='')
-				# traceback.print_exc()
+				traceback.print_exc()
 				continue
 		print('ERROR: "{}" and "{}"\n'.format(first, second), end='')
 	except Exception as e:
 		print('ERROR: "{}" and "{}": {}\n'.format(first, second, e), end='')
-		# traceback.print_exc()
+		traceback.print_exc()
 
 	return count
 
 # search PubMed for paper and the count and search results to 
 def search(first_keyword, second_keyword, exclude):
-	mat = np.full((len(first_keyword.keys()), len(second_keyword.keys())+1), None)
-
-	with open('PubMed/_index.txt', 'w', encoding='utf-8') as f:
-		with ThreadPoolExecutor(max_workers=3) as executor:
-			for i in range(mat.shape[0]):
-				if not os.path.exists('PubMed/A{}'.format(i)):
-					os.mkdir('PubMed/A{}'.format(i))
-				mat[i][0] = first_keyword[i]
-				for j in range(1, mat.shape[1]):
-					f.write('"{}", "{}": A{}/B{}\n'.format(first_keyword[i], second_keyword[j-1], i, j-1))
-					# check if combination should be excluded
-					if '{} {}'.format(first_keyword[i], second_keyword[j-1]) in exclude:
-						mat[i][j] = 0
-					else:
-						if not os.path.exists('PubMed/A{}/B{}'.format(i, j-1)):
-							os.mkdir('PubMed/A{}/B{}'.format(i, j-1))
-						mat[i][j] = (executor.submit(worker, first_keyword[i], second_keyword[j-1], 'PubMed/A{}/B{}'.format(i, j-1)))
-
-	return mat
+	# multithreading
+	for k1 in first_keyword:
+		for k2 in second_keyword:
+			if '{} {}'.format(k1, k2) not in exclude:
+				executor.submit(worker, k1, k2)
 
 # write search counts to csv
-def write_result_count(first_keyword, second_keyword, mat):
-	import pandas as pd 
-	np.savetxt('raw_search_count.csv', mat, delimiter=',', header=','.join(['']+[second_keyword[i] for i in range(len(second_keyword.keys()))]), fmt=['%s'] + ['%d']*len(second_keyword.keys()), encoding='utf-8')
-	df = pd.read_csv('raw_search_count.csv', header=0, index_col=0)
-	sample = pd.read_excel(sample_format, header=1, index_col=1, sheet_name='Clustered data')
-	sample = sample.drop(columns=['Unnamed: 0'])
-	df = df.transpose()
-	reordered = df.loc[sample.index, sample.columns]
-	reordered.to_csv('clustered_search_count.csv')
+def write_result_count(first_keyword, second_keyword):
+	import csv
+	conn = sqlite3.connect('./pubmed.db')
+	c = conn.cursor()
+	with open('search_count.csv', 'w') as file:
+		#writer = csv.writer(file)
+		# label first row
+		#writer.writerow(['']+first_keyword)
+		print(['']+first_keyword)
+		for k2 in second_keyword:
+			c.execute("SELECT COUNT(TITLE) FROM PubMed WHERE K2=? AND K1=(SELECT K1 FROM PubMed)", (k2))
+			print([k2]+c.fetchall())
+			#writer.writerow([k2]+)
+	conn.close()
+	'''
+	c.execute("SELECT * FROM PubMed")
+	print(c.fetchall())
+	c.execute("SELECT * FROM PubMed_Abstract")
+	print(c.fetchall())
+	'''
 
 def test():
-	if not os.path.exists('test'):
-		os.mkdir('test')
+	first_keyword = ['abdominal injuries']
+	second_keyword = ['C-reactive protein', 'bilirubin']
 	keyword_combo = [
-		("hollow viscus injury", "pathogenic")
+		('abdominal injuries', 'C-reactive protein'),
+		('abdominal injuries', 'bilirubin'),
+		#("hollow viscus injury", "pathogenic"), # single return and book
+		#('physical trauma', 'bilirubin')
 	]
 	for k in keyword_combo:
-		if not os.path.exists('test/{}_{}'.format(k[0], k[1])):
-			os.mkdir('test/{}_{}'.format(k[0], k[1]))
-		worker(*(k), 'test/{}_{}'.format(k[0], k[1]))
+		worker(*(k))
+	write_result_count(first_keyword, second_keyword)
 
 def main():
+	setup_database()
+	test()
+	'''
 	first_keyword, second_keyword, exclude = keyword()
-	if not os.path.exists('PubMed'):
-		os.mkdir('PubMed')
-	mat = search(first_keyword, second_keyword, exclude)
-	mat = unpack(mat)
-	write_result_count(first_keyword, second_keyword, mat)
+	search(first_keyword, second_keyword, exclude)
+	'''
+	write_result_count(first_keyword, second_keyword)
 
 if __name__ == '__main__':
 	Entrez.email = 'david.shaw@nyu.edu'
