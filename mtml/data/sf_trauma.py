89749b8e651c6f7a25b652122fb167bc32a82b92
Derek Huang <djh458@stern.nyu.edu>
2020-10-02 02:25:29 -0400

    add NA dropping capability to spl_factory in sf_trauma.py + passed unit tests

--- mtml/data/sf_trauma.py
+++ mtml/data/sf_trauma.py
@@ -30,8 +30,9 @@ from .. import sf_num_data_prep_path, vitals_cols
 
 
 def spl_factory(inputs, targets, target_transform = None,
-                target_transform_kwargs = None, test_size = 0.25,
-                shuffle = True, random_state = None):
+                target_transform_kwargs = None, dropna = False, na_axis = 0,
+                na_how = "any", na_thresh = None, na_subset = None,
+                test_size = 0.25, shuffle = True, random_state = None):
     """Factory for generating supervised problems using different data.
     
     Uses :func:`~sklearn.model_selection.train_test_split` for splitting data
@@ -52,6 +53,16 @@ def spl_factory(inputs, targets, target_transform = None,
     :type target_transform: function, optional
     :param target_transform_kwargs: Keyword arguments to pass to ``transform``.
     :type target_transform_kwargs: dict, optional
+    :param dropna: ``True`` to drop missing values. Default ``False``.
+    :type dropna: bool, optional
+    :param na_axis: ``axis`` parameter of :meth:`pandas.DataFrame.dropna`.
+    :type na_axis: int, optional
+    :param na_how: ``how`` parameter of :meth:`pandas.DataFrame.dropna`.
+    :type na_how: str, optional
+    :param na_thresh: ``thresh`` parameter of :meth:`pandas.DataFrame.dropna`.
+    :type na_thresh: int, optional
+    :param na_subset: ``subset`` parameter of :meth:`pandas.DataFrame.dropna`.
+    :type na_subset: array-like, optional
     :param test_size: Float in ``[0, 1]`` to represent fraction of points to
         use for validation data or an int to represent number of points to use
         for validation data. Passed directly to
@@ -79,10 +90,17 @@ def spl_factory(inputs, targets, target_transform = None,
         target_transform_kwargs = {}
     # load data/prep/sf_trauma_data_num.csv
     df = pd.read_csv(sf_num_data_prep_path)
-    # extract inputs columns
-    X = df.loc[:, inputs]
-    # choose targets and apply transform
-    y = target_transform(df.loc[:, targets], **target_transform_kwargs)
+    # extract input and target columns
+    data = df.loc[:, inputs].join(df.loc[:, targets])
+    # drop NA values according to NA dropping strategy
+    if dropna == True:
+        data = data.dropna(axis = na_axis, how = na_how, thresh = na_thresh,
+                           subset = na_subset)
+    # split into X and y and apply transform to y
+    X = data.loc[:, inputs]
+    y = data.loc[:, targets]
+    # apply transform to targets
+    y = target_transform(y, **target_transform_kwargs)
     # flatten if shape is not 1D. more than 2D output is not supported.
     if len(y.shape) == 1:
         pass
@@ -98,7 +116,9 @@ def spl_factory(inputs, targets, target_transform = None,
     return tuple(out)
 
 
-def cls_vitals_trauma(test_size = 0.25, shuffle = True, random_state = None):
+def cls_vitals_trauma(test_size = 0.25, shuffle = True, dropna = False,
+                      na_axis = 0, na_how = "any", na_thresh = None,
+                      na_subset = None, random_state = None):
     """Return vitals data from ``sf_num_data_prep_path`` to classify trauma.
     
     Trauma is indicated when ISS score is greater than 15.
@@ -112,6 +132,16 @@ def cls_vitals_trauma(test_size = 0.25, shuffle = True, random_state = None):
         defaults to ``True``. ``False`` leaves split data arranged in ascending
         index order.
     :type shuffle: bool, optional
+    :param dropna: ``True`` to drop missing values. Default ``False``.
+    :type dropna: bool, optional
+    :param na_axis: ``axis`` parameter of :meth:`pandas.DataFrame.dropna`.
+    :type na_axis: int, optional
+    :param na_how: ``how`` parameter of :meth:`pandas.DataFrame.dropna`.
+    :type na_how: str, optional
+    :param na_thresh: ``thresh`` parameter of :meth:`pandas.DataFrame.dropna`.
+    :type na_thresh: int, optional
+    :param na_subset: ``subset`` parameter of :meth:`pandas.DataFrame.dropna`.
+    :type na_subset: array-like, optional
     :param random_state: Int seed or :class:`~numpy.random.RandomState` to use
         to make multiple calls to this method reproducible. Passed directly to
         :func:`~sklearn.model_selection.train_test_split`.
@@ -125,12 +155,16 @@ def cls_vitals_trauma(test_size = 0.25, shuffle = True, random_state = None):
         return np.array(list(map(lambda x: 1 if x > 15 else 0, ar.values)))
     # use factory method
     return spl_factory(vitals_cols, ["iss"],
-                       target_transform = make_trauma_feature,
+                       target_transform = make_trauma_feature, dropna = dropna,
+                       na_axis = na_axis, na_how = na_how,
+                       na_thresh = na_thresh, na_subset = na_subset,
                        test_size = test_size, shuffle = shuffle,
                        random_state = random_state)
 
 
-def cls_vitals_mort(test_size = 0.25, shuffle = True, random_state = None):
+def cls_vitals_mort(test_size = 0.25, shuffle = True, dropna = False,
+                      na_axis = 0, na_how = "any", na_thresh = None,
+                      na_subset = None, random_state = None):
     """Return vitals data from ``sf_num_data_prep_path`` to classify mortality.
     
     Mortality corresponds to mortality at discharge.
@@ -144,6 +178,16 @@ def cls_vitals_mort(test_size = 0.25, shuffle = True, random_state = None):
         defaults to ``True``. ``False`` leaves split data arranged in ascending
         index order.
     :type shuffle: bool, optional
+    :param dropna: ``True`` to drop missing values. Default ``False``.
+    :type dropna: bool, optional
+    :param na_axis: ``axis`` parameter of :meth:`pandas.DataFrame.dropna`.
+    :type na_axis: int, optional
+    :param na_how: ``how`` parameter of :meth:`pandas.DataFrame.dropna`.
+    :type na_how: str, optional
+    :param na_thresh: ``thresh`` parameter of :meth:`pandas.DataFrame.dropna`.
+    :type na_thresh: int, optional
+    :param na_subset: ``subset`` parameter of :meth:`pandas.DataFrame.dropna`.
+    :type na_subset: array-like, optional
     :param random_state: Int seed or :class:`~numpy.random.RandomState` to use
         to make multiple calls to this method reproducible. Passed directly to
         :func:`~sklearn.model_selection.train_test_split`.
@@ -152,12 +196,16 @@ def cls_vitals_mort(test_size = 0.25, shuffle = True, random_state = None):
         :class:`numpy.ndarray` objects
     :rtype: tuple
     """
-    return spl_factory(vitals_cols, ["mortality at disch"],
+    return spl_factory(vitals_cols, ["mortality at disch"], dropna = dropna,
+                       na_axis = na_axis, na_how = na_how,
+                       na_thresh = na_thresh, na_subset = na_subset,
                        test_size = test_size, shuffle = shuffle,
                        random_state = random_state)
 
 
-def cls_vitals_mof(test_size = 0.25, shuffle = True, random_state = None):
+def cls_vitals_mof(test_size = 0.25, shuffle = True, dropna = False,
+                      na_axis = 0, na_how = "any", na_thresh = None,
+                      na_subset = None, random_state = None):
     """Return vitals data from ``sf_num_data_prep_path`` to classify MOF.
     
     MOF = multiple organ failure.
@@ -171,6 +219,16 @@ def cls_vitals_mof(test_size = 0.25, shuffle = True, random_state = None):
         defaults to ``True``. ``False`` leaves split data arranged in ascending
         index order.
     :type shuffle: bool, optional
+    :param dropna: ``True`` to drop missing values. Default ``False``.
+    :type dropna: bool, optional
+    :param na_axis: ``axis`` parameter of :meth:`pandas.DataFrame.dropna`.
+    :type na_axis: int, optional
+    :param na_how: ``how`` parameter of :meth:`pandas.DataFrame.dropna`.
+    :type na_how: str, optional
+    :param na_thresh: ``thresh`` parameter of :meth:`pandas.DataFrame.dropna`.
+    :type na_thresh: int, optional
+    :param na_subset: ``subset`` parameter of :meth:`pandas.DataFrame.dropna`.
+    :type na_subset: array-like, optional
     :param random_state: Int seed or :class:`~numpy.random.RandomState` to use
         to make multiple calls to this method reproducible. Passed directly to
         :func:`~sklearn.model_selection.train_test_split`.
@@ -179,5 +237,7 @@ def cls_vitals_mof(test_size = 0.25, shuffle = True, random_state = None):
         :class:`numpy.ndarray` objects
     :rtype: tuple
     """
-    return spl_factory(vitals_cols, ["mof"], test_size = test_size,
+    return spl_factory(vitals_cols, ["mof"], dropna = dropna, na_axis = na_axis,
+                       na_how = na_how, na_thresh = na_thresh,
+                       na_subset = na_subset,test_size = test_size,
                        shuffle = shuffle, random_state = random_state)
--- mtml/tests/test_vitals_factory.py
+++ mtml/tests/test_vitals_factory.py
@@ -11,8 +11,8 @@ from ..data.sf_trauma import cls_vitals_mof, cls_vitals_mort, cls_vitals_trauma
 @pytest.mark.parametrize("seed", [7])
 @pytest.mark.parametrize("func", [cls_vitals_trauma, cls_vitals_mort,
                                   cls_vitals_mof])
-def test_vitals_factory(func, seed):
-    """Test data sets created using :func:`mtml.data.sf_trauma.vitals_factory`.
+def test_factory_vitals_with_na(func, seed):
+    """Test functions using vitals data as input data, keeping NA data.
     
     Check shape, column names, output.
     """
@@ -26,6 +26,29 @@ def test_vitals_factory(func, seed):
     assert y_train.shape == (1120,)
     assert X_val.shape == (374, 9)
     assert y_val.shape == (374,)
+    assert X_train.shape[0] == y_train.shape[0]
+    assert X_val.shape[0] == y_val.shape[0]
+
+
+@pytest.mark.parametrize("seed", [7])
+@pytest.mark.parametrize("func", [cls_vitals_trauma, cls_vitals_mort,
+                                  cls_vitals_mof])
+def test_factory_vitals_without_na(func, seed):
+    """Test functions using vitals data as input data, dropping any NA data.
+    
+    Check shape, column names, output.
+    """
+    # seed for reproducibility
+    X_train, X_val, y_train, y_val = func(dropna = True, random_state = seed)
+    # check columns
+    assert list(X_train.columns) == list(X_val.columns)
+    assert list(X_train.columns) == list(vitals_cols)
+    # check shapes (note the data set is much smaller)
+    # using different functions gives slightly different NA droppage
+    assert X_train.shape == (240, 9) or X_train.shape == (242, 9)
+    assert y_train.shape == (240,) or y_train.shape == (242,)
+    assert X_val.shape == (81, 9)
+    assert y_val.shape == (81,)
     assert X_train.shape[0] == y_train.shape[0]
     assert X_val.shape[0] == y_val.shape[0]
     
