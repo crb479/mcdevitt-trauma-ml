19b556964e05eab89f1c4ad16e386fb76ee6fade
Cameron Breze <crb479@nyu.edu>
2020-10-09 19:52:50 -0400

    work on demo task

--- mtml/toy/crb479.py
+++ mtml/toy/crb479.py
@@ -1,1 +1,7 @@
-# test
+# For Demo Task
+
+def add_user_tag(__user_tag__):
+	def decorator_set_user_tag(func):
+		func.__user_tag__ = __user_tag__
+		return func
+	return decorator_set_user_tag
---
+++ mtml/toy/tests/test_crb479.py
@@ -0,0 +1,6 @@
+# Testing for Demo Task
+
+# from ??? import echo_args
+
+def test_crb479():
+	return
