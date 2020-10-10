e5b141b926693df2fdf77486db1b81c55b0fa7fb
Cameron Breze <crb479@nyu.edu>
2020-10-10 07:44:22 -0400

    completed crb479.py + passed test_crb479.py

--- mtml/toy/crb479.py
+++ mtml/toy/crb479.py
@@ -1,7 +1,12 @@
 # For Demo Task
+__doc__ = "Decorator fxn to add '__user_tag__' to non-built-in Python fxn"
 
-def add_user_tag(__user_tag__):
-	def decorator_set_user_tag(func):
-		func.__user_tag__ = __user_tag__
-		return func
-	return decorator_set_user_tag
+def add_user_tag(fxn):
+	'''Adds username as an attribute ``__user_tag__`` to ``fxn``.
+    
+    :param fxn: Function to decorate
+    :type fxn: function
+    :rtype: function
+	'''
+	fxn.__user_tag__ = 'crb479'
+	return fxn
--- mtml/toy/tests/test_crb479.py
+++ mtml/toy/tests/test_crb479.py
@@ -1,6 +1,16 @@
 # Testing for Demo Task
 
-# from ??? import echo_args
+__doc__ = "Test of mtml.toy.crb479.add_user_tag."
+
+from .. import echo_args
+from ..crb479 import add_user_tag
 
 def test_crb479():
-	return
+	'''Test 'mtml.toy.crb479.add_user_tag'fxn by wrapping 'echo_args'
+	'''
+	wrapped_echo_args = add_user_tag(echo_args)
+	# Evaluate with arbitrary arguments
+	print(wrapped_echo_args('apple', 'orange', data = {'num': 1, 'letter': 'a'}))
+	# Assert that 'wrapped_echo_args' has desired attribute
+	assert(hasattr(wrapped_echo_args, '__user_tag__') and 
+		(wrapped_echo_args.__user_tag__ == 'crb479'))
