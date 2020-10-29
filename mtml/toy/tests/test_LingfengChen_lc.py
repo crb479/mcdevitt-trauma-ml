9a6ba27c4ee1282fa6b32bf1cf3c3dc7528bfeb4
Liam <lc3651@nyu.edu>
2020-10-29 13:02:48 +0800

    completed my_uname.py + passed test_my_uname.py

---
+++ mtml/toy/tests/test_LingfengChen_lc.py
@@ -0,0 +1,7 @@
+from .. import echo_args
+from ..LingfengChen_lc import add_user_tag
+
+def test_LingfengChen_lc():
+    f = add_user_tag(echo_args)
+    print(f('A', 'B', 'C'))
+    assert f.__user_tag__ == 'LingfengChen_lc'
