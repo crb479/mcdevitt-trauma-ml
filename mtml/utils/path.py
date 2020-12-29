8fab1d49619d7837b5ce4a115beed6c79153d179
Derek Huang <djh458@stern.nyu.edu>
2020-12-29 07:18:05 -0500

    added find_results_home_ascending to path.py in mtml/utils to help find results directory when running scripts in slurm + update slurm/djh458/run_whitened_pca.py

---
+++ mtml/utils/path.py
@@ -0,0 +1,47 @@
+__doc__ = "Path-related utilities."
+
+import os
+import os.path
+
+
+def find_results_home_ascending(dirname = "results"):
+    """Go up the directory tree looking for the results home directory.
+
+    .. warning::
+
+       Only tested to work correctly on POSIX-like systems. Not sure what will
+       happen on Windows, where the path separator is reversed.
+
+    Typical use case is when executing a script on NYU HPC from the ``slurm``
+    directory. There's no way to start up a package and somehow precompute the
+    location of the ``results`` directory, but we can search up the directory
+    tree until we find it. If we fail, :class:`FileNotFoundError` is raised.
+
+    :param dirname: Name of the results home directory. This should not need to
+        be changed at all.
+    :type dirname: str, optional
+    :raises FileNotFoundError: If the results home directory can't be found by
+        traversing up the directory tree.
+    :returns: Absolute path to the results home directory.
+    :rtype: str
+    """
+    # get current directory (absolute path)
+    cur_dir = os.path.abspath(".")
+    # while we haven't reached the root directory yet (path is empty)
+    while cur_dir != "":
+        # get list of files in this directory
+        files = os.listdir(cur_dir)
+        # if our desired directory name is in files and it is a directory, then
+        # add "/" + dirname to cur_dir and break
+        if dirname in files and os.path.isdir(dirname):
+            cur_dir = f"{cur_dir}/{dirname}"
+            break
+        # else, we drop one more directory level
+        cur_dir = "/".join(cur_dir.split("/")[:-1])
+    # if cur_dir is empty, we failed, so raise error
+    if cur_dir == "":
+        raise FileNotFoundError(
+            f"couldn't find dir {dirname} by walking up the directory tree"
+        )
+    # else return, we are done
+    return cur_dir
--- slurm/djh458/run_whitened_pca.py
+++ slurm/djh458/run_whitened_pca.py
@@ -4,19 +4,27 @@ import os.path
 
 # pylint: disable=import-error,relative-beyond-top-level
 from mtml.modeling.vte.decomposition import whitened_pca
+from mtml.utils.path import find_results_home_ascending
 from mtml.utils.persist import persist_json, persist_pickle, remove_all_persist
 
-from .. import RESULTS_HOME
 
+# attempt to find the results top-level directory
+results_home = find_results_home_ascending()
 
-# add persistence decorators and run
-persist_json(
-    target = RESULTS_HOME + "/djh458/vte_whitened_pca_params.json",
+# persistence decorator args
+persist_json_args = dict(
+    target = results_home + "/djh458/vte_whitened_pca_params.json",
     out_transform = lambda x: (x["pcas"][0].get_params(),
                                x["pcas"][1].get_params())
-)(
-    persist_pickle(
-        target = RESULTS_HOME + "/djh458/vte_whitened_pcas.pickle",
-        out_transform = lambda x: x["pcas"]
-    )(remove_all_persist(whitened_pca))
-)(report = True, random_seed = 7)
+)
+persist_pickle_args = dict(
+    target = results_home + "/djh458/vte_whitened_pcas.pickle",
+    out_transform = lambda x: x["pcas"]
+)
+# add persistence decorators and run
+task = persist_json(**persist_json_args)(
+    persist_pickle(**persist_pickle_args)(
+        remove_all_persist(whitened_pca)
+    )
+)
+_ = task(report = True, random_seed = 7)
