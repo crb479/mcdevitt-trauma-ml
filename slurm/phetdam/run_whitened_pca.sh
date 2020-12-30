e1f83b7b2ea6cfb10897ffc7c371c95abf00636c
Derek Huang <djh458@stern.nyu.edu>
2020-12-29 22:50:14 -0500

    update --mem arg for run_whitened_pca.sh; should not be running out of memory

--- slurm/phetdam/run_whitened_pca.sh
+++ slurm/phetdam/run_whitened_pca.sh
@@ -3,14 +3,16 @@
 #
 # note that this job usually completes in a couple seconds, hence the small
 # value of time used. checking with seff also shows that not even a couple of
-# MB are needed by this job so i pass in --mem (memory per node).
+# MB are needed by this job so i pass in --mem (memory per node). however, you
+# can't pass in a really small number (get OUT_OF_MEMORY error for some reason),
+# so i've chosen to set it at 100 MB.
 #
 # there is only one CPU that is needed since everything is run sequentially and
-# thus there is also only one task (defaulta, no need to specify).
+# thus there is also only one task (defaults, no need to specify).
 
 #SBATCH --job-name=djh458:whitened_pca
-#SBATCH --mem=2M
-#SBATCH --time=00:00:10
+#SBATCH --mem=100M
+#SBATCH --time=00:00:05
 #SBATCH --cpus-per-task=1
 #SBATCH --nodes=1
 
