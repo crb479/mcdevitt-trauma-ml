96949fea781fa4f69df20ea764596467f4bea494
Derek Huang <djh458@stern.nyu.edu>
2021-01-04 01:18:42 -0500

    add update run_whitened_kernel_pca.sh and its similarly named JSON config file for SLURMCluster

--- slurm/phetdam/run_whitened_kernel_pca.sh
+++ slurm/phetdam/run_whitened_kernel_pca.sh
@@ -1,24 +1,15 @@
 #!/bin/bash
 # slurm batch job to run run_whitened_kernel_pca.py
 #
-# GridSearchCV uses loky for multiprocessing so the total number of processes
-# that will be spawned is 4 * cv_folds (there are 4 kernels being tested).
-# however, slurm views the processes being spawned by the Python interpreter as
-# being part of the same "task", so we are forced to set nodes + tasks to 1 and
-# rather use 12 cores on the node for parallel execution.
-#
-# the problem here is that we can run in parallel but not in a distributed
-# fashion (limited to a single node). memory use is ~3 GB, so we pass mem too.
-#
-# runtime around 15 minutes? i'll check later (todo)
+# 20 minutes walltime should be enough. "master" for distributed workers.
 
 #SBATCH --job-name=djh458:whitened_kernel_pca
 #SBATCH --output=whitened_kernel_pca.out
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=12
-#SBATCH --mem=3G
-#SBATCH --time=00:10:00
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=100M
+#SBATCH --time=00:20:00
 
 # metric, cv fold count (cores/task should equal this * 4). note that we pass
 # the cores/task (exported by default as SLURM_CPUS_PER_TASK) to n_jobs so we
@@ -32,6 +23,6 @@ VERBOSITY=1
 # specify metric, number of CV folds, and number of processes to spawn
 source ~/djh458/bin/activate
 python3 ~/mcdevitt-trauma-ml/slurm/phetdam/run_whitened_kernel_pca.py \
-    --metric=$METRIC --cv-folds=$CV_FOLDS --njobs=$SLURM_CPUS_PER_TASK \
-    --verbose=$VERBOSITY
+    --metric=$METRIC --cv-folds=$CV_FOLDS --njobs=$CV_FOLDS \
+    --jobqueue-config=./run_whitened_kernel_pca.json --verbose=$VERBOSITY
 deactivate
