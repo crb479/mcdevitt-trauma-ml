cf437513e090e9590c1ba977cd639280463e3634
Derek Huang <djh458@stern.nyu.edu>
2021-01-14 18:12:13 -0500

    update run_whitened_kernel_pca.sh to give master more memory + reduce worker memory in run_whitened_kernel_pca.json

--- slurm/phetdam/run_whitened_kernel_pca.sh
+++ slurm/phetdam/run_whitened_kernel_pca.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
 # slurm batch job to run run_whitened_kernel_pca.py
 #
-# 20 minutes walltime should be enough. "master" for distributed workers.
+# 20 minutes walltime should be enough. "master" for distributed workers. note
+# that when greene becomes busy walltime might need to be extended.
 
 #SBATCH --job-name=djh458:whitened_kernel_pca
 #SBATCH --output=whitened_kernel_pca.out
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=1G
+#SBATCH --mem=2G
 #SBATCH --time=00:45:00
 
-# metric, cv fold count (cores/task should equal this * 4). note that we pass
-# the cores/task (exported by default as SLURM_CPUS_PER_TASK) to n_jobs so we
-# control the number of processes spawned by joblib.
+# metric, cv fold count (total fits equals this * 4)
 METRIC="f1_score"
 CV_FOLDS=3
 # GridSearchCV verbosity (1 default)
 VERBOSITY=1
 
-# activate venv, run, deactivate. pass $METRIC, $CV_FOLDS, and $SLURM_NTASKS to
-# specify metric, number of CV folds, and number of processes to spawn
+# source the master memmonitor so that it has access to the slurm env (TBA)
+
+# activate venv, run, deactivate. pass $METRIC, $CV_FOLDS to specify scoring
+# metric and number of cv folds to script. with --jobqueue-config specified,
+# --njobs specifies the number of worker processes dask_jobqueue will start.
 source ~/djh458/bin/activate
 python3 ~/mcdevitt-trauma-ml/slurm/phetdam/run_whitened_kernel_pca.py \
-    --metric=$METRIC --cv-folds=$CV_FOLDS --njobs=1 \
+    --metric=$METRIC --cv-folds=$CV_FOLDS --njobs=2 \
     --jobqueue-config=./run_whitened_kernel_pca.json \
     --mmap-dir=/scratch/djh458 --verbose=$VERBOSITY
 deactivate
