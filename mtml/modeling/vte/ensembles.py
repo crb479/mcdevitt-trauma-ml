2513f6a7513814dbd3f8018e5344c30ad4816996
David Shaw <mx496@nyu.edu>
2021-01-14 21:43:52 -0500

    Add files via upload

---
+++ mtml/modeling/vte/ensembles.py
@@ -0,0 +1,81 @@
+import numpy as np
+import pandas as pd
+
+# A host of Scikit-learn models
+from sklearn.svm import SVC, LinearSVC
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.kernel_approximation import Nystroem
+from sklearn.kernel_approximation import RBFSampler
+from sklearn.pipeline import make_pipeline
+from sklearn.metrics import roc_auc_score
+
+def get_models():
+    """Generate a library of base learners."""
+    nb = GaussianNB()
+    svc = SVC(C=100, probability=True)
+    knn = KNeighborsClassifier(n_neighbors=3)
+    lr = LogisticRegression(C=100, random_state=SEED)
+    nn = MLPClassifier((80, 10), early_stopping=False, random_state=SEED)
+    gb = GradientBoostingClassifier(n_estimators=100, random_state=SEED)
+    rf = RandomForestClassifier(n_estimators=10, max_features=3, random_state=SEED)
+
+    models = {'svm': svc,
+              'knn': knn,
+              'naive bayes': nb,
+              'mlp-nn': nn,
+              'random forest': rf,
+              'gbm': gb,
+              'logistic': lr,
+              }
+
+    return models
+
+
+def train_predict(model_list, xtrain, ytrain, xtest, ytest):
+    """Fit models in list on training set and return preds"""
+    P = np.zeros((ytest.shape[0], len(model_list)))
+    P = pd.DataFrame(P)
+
+    print("Fitting models.")
+    cols = list()
+    for i, (name, m) in enumerate(models.items()):
+        print("%s..." % name, end=" ", flush=False)
+        m.fit(xtrain, ytrain)
+        P.iloc[:, i] = m.predict_proba(xtest)[:, 1]
+        cols.append(name)
+        print("done")
+
+    P.columns = cols
+    print("Done.\n")
+    return P
+
+
+def score_models(P, y):
+    """Score model in prediction DF"""
+    print("Scoring models.")
+    for m in P.columns:
+        score = roc_auc_score(y, P.loc[:, m])
+        print("%-26s: %.3f" % (m, score))
+    print("Done.\n")
+
+from mtml import VTE_CONT_INPUT_COLS, VTE_OUTPUT_COLS, VTE_QIDA_INPUT_COLS
+from mtml.data.vte import vte_slp_factory
+from mtml.modeling.vte.data_transforms import replace_hdl_tot_chol_with_ratio
+
+random_seed = 0
+
+X_train, X_test, y_train, y_test = vte_slp_factory(
+    data_transform = replace_hdl_tot_chol_with_ratio,
+    inputs = VTE_QIDA_INPUT_COLS, targets = VTE_OUTPUT_COLS, dropna = True,
+    random_state = random_seed
+)
+
+SEED = 0
+
+models = get_models()
+P = train_predict(models, X_train, y_train, X_test, y_test)
+score_models(P, y_test)
